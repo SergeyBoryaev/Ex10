5904442f1de089b6bb8b0c9b5300544d72ac44be
Sergey Boryaev <somsom.som2@yandex.ru>
2021-01-19 09:53:45 +0300

    first try

--- include/MyStack.h
+++ include/MyStack.h
@@ -1,9 +1,69 @@
-// Copyright 2020 A.SHT
+// Copyright 2021 GHA Test Team
 #ifndef INCLUDE_MYSTACK_H_
 #define INCLUDE_MYSTACK_H_
+#include <iostream>
+#include <string>
 
-template<class T>
+template <typename T>
 class MyStack {
+ private:
+  unsigned max;
+  unsigned current;
+  T* stack;
+
+ public:
+  MyStack() {
+    max = 0;
+    current = 0;
+    stack = nullptr;
+  }
+
+  explicit MyStack(unsigned size) {
+    max = size;
+    current = 0;
+    stack = new T[size];
+  }
+
+  explicit MyStack(const MyStack& b) {
+    max = b.max;
+    current = b.current;
+    stack = new T[max];
+    for (unsigned i = 0; i < current; i++)
+      stack[i] = b.stack[i];
+  }
+
+  ~MyStack() {
+    delete stack;
+  }
+
+  T get() const {
+    if (!isEmpty())
+      return stack[current-1];
+    return 0;
+  }
+
+  T pop() {
+    if (!isEmpty()) {
+      current--;
+      return stack[current];
+    }
+    return 0;
+  }
+
+  void push(T var) {
+    if (current < max) {
+      stack[current] = var;
+      current++;
+    }
+  }
+
+  bool isFull() const {
+    return max == current;
+  }
+
+  bool isEmpty() const {
+    return current == 0;
+  }
 };
 
 #endif  // INCLUDE_MYSTACK_H_
--- src/postfix.cpp
+++ src/postfix.cpp
@@ -3,5 +3,42 @@
 #include "postfix.h"
 
 std::string infix2postfix(std::string infix) {
+  MyStack<char> oper(infix.size());
+  MyStack<char> var(infix.size());
+  for(auto i : infix) {
+    if (i == '(')
+      oper.push(i);
+    else if (i == ')') {
+      while (oper.get() != '(')
+        var.push(oper.pop());
+      oper.pop();
+    }
+    else if (i >= '0' && i <= '9')
+      var.push(i);
+    else if (i == '+' || i == '-') 
+      if(oper.isEmpty() || oper.get() == '(')
+        oper.push(i);
+      else {
+        while (!(oper.isEmpty() || oper.get() == '('))
+          var.push(oper.pop());
+        oper.push(i);
+      }
+    else if (i == '*' || i == '/')
+      if (oper.isEmpty() || (oper.get() != '*' && oper.get() != '/'))
+        oper.push(i);
+      else {
+        while (!(oper.isEmpty() || (oper.get() != '*' && oper.get() != '/')))
+          var.push(oper.pop());
+        oper.push(i);
+      }
+  }
+  while(!oper.isEmpty())
+    var.push(oper.pop());
+  infix.clear();
+  while(!var.isEmpty()) {
+    infix.insert(infix.begin(), ' ');
+    infix.insert(infix.begin(), var.pop());
+  }
+  infix.pop_back();
   return infix;
 }
